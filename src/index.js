4be28e0e5db37b1b6de92aff85f6616b8426e65e
Jennifer Huynh <jntlhuynh@gmail.com>
2022-01-17 22:55:27 -0800

    added more comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 // create the factory for todos object
 // similar to ticktick
 //Brainstorm what kind of properties your todo-items are going to have. At a minimum they should have a title, description, dueDate and priority. You might also want to include notes or even a checklist.
+
+//Your todo list should have projects or separate lists of todos. When a user first opens the app, there should be some sort of ‘default’ project to which all of their todos are put. Users should be able to create new projects and choose which project their todos go into.

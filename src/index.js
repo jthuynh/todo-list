a4586ba8af4efd59889cbb368dfaa77a8f6f89cb
Jennifer Huynh <jntlhuynh@gmail.com>
2022-01-17 22:54:48 -0800

    added comments to brainstorm

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// create the factory for todos object
+// similar to ticktick
+//Brainstorm what kind of properties your todo-items are going to have. At a minimum they should have a title, description, dueDate and priority. You might also want to include notes or even a checklist.
